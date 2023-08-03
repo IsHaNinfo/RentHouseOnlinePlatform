db8bdeb38ec9e1b8ff51d94aedd8bb59d87c9f78
ishaninfo56@gmail.com <120440668+IsHaNinfo@users.noreply.github.com>
2023-08-03 09:11:28 +0530

    add the search bar

--- frontend/src/Components/ListView/Home.js
+++ frontend/src/Components/ListView/Home.js
@@ -13,58 +13,132 @@ import HouseIcon from "@mui/icons-material/House";
 import SearchIcon from "@mui/icons-material/Search";
 import LanguageIcon from "@mui/icons-material/Language";
 import Login from "../Userpages/Login";
+import styled, { css } from "styled-components";
+import { useMediaQuery } from "@mui/material";
+import SearchBar from "./SearchBar";
+
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
-  const [loginOpen, setLoginOpen] = useState(false); // State to control Login component's open state
+  const [loginOpen, setLoginOpen] = useState(false);
+  const [languageOpen, setLanguageOpen] = useState(false);
+  const [addPostOpen, setAddPostOpen] = useState(false);
 
   const handleChange = (event) => {
     setInputValue(event.target.value);
   };
   const handleLoginClick = () => {
-    setLoginOpen(true); // Open the Login component when the login button is clicked
+    setLoginOpen(true);
   };
   const handleLoginClose = () => {
-    setLoginOpen(false); // Close the Login component
+    setLoginOpen(false);
   };
+  const handleLanguageClick = () => {
+    setLanguageOpen(!languageOpen);
+  };
+  const handleAddPostClick = () => {
+    setAddPostOpen(!addPostOpen);
+  };
+
+  const AppBarContainer = styled.div`
+    margin-top: 20px;
+    .MuiAppBar-root {
+      background-color: white;
+      box-shadow: none;
+    }
+  `;
+
+  const isLargeScreen = useMediaQuery("(min-width: 768px)");
+
+  const MainContainner = styled(Grid)`
+    margin-left: ${isLargeScreen ? "5px" : "5px"};
+    margin-right: ${isLargeScreen ? "5px" : "15px"};
+  `;
+
+  const LoginGrid = styled(Grid)`
+    color: green;
+    border: 1px solid #14ce9e;
+    width: 174px;
+    height: 51px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: ${isLargeScreen ? "15px" : "35px"};
+  `;
+
+  const PostGrid = styled(Grid)`
+    background-color: #14ce9e;
+    font-size: 45px;
+    border: 1px solid #14ce9e;
+    width: 174px;
+    height: 51px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-left: ${isLargeScreen ? "5px" : "35px"};
+  `;
+
+  const StyledHouseIcon = styled(HouseIcon)`
+    color: #14ce9e;
+    ${(props) => css`
+      font-size: ${props.fontSize || "45px"};
+    `}
+  `;
+
+  const Title = styled.div`
+    font-weight: bold;
+    color: black;
+    margin-left: ${isLargeScreen ? "5px" : "35px"};
+    margin-right: ${isLargeScreen ? "5px" : "15px"};
+    display: ${isLargeScreen ? "block" : "none"};
+  `;
+
+  const Input = styled(TextField)`
+    width: ${isLargeScreen ? "600px" : "300px"};
+    margin-top: 5px;
+    margin-bottom: 5px;
+    height: 60px;
+    margin-right: ${isLargeScreen ? "15px" : "35px"};
+    border-radius: 20px 20px;
+    border: 0px solid red; /* Change border style here */
+  `;
+
+  const LoginB = styled(Button)`
+    color: black;
+    font-size: 35px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
+
+  const StyledLanguageIcon = styled(LanguageIcon)`
+    color: #14ce9e;
+    border: 1px solid #14ce9e;
+    ${(props) => css`
+      font-size: ${props.fontSize || "45px"};
+    `}
+  `;
 
   return (
     <div>
-      <AppBar
-        position="static"
-        sx={{ backgroundColor: "white", boxShadow: "none", mt: "15px" }}
-      >
+      <SearchBar />
+      {/* <AppBarContainer position="static">
         <Toolbar>
-          <Grid container alignItems="center" sx={{ ml: "35px", mr: "15px" }}>
+          <MainContainner container alignItems="center">
             <Grid item>
               <Button>
-                <HouseIcon sx={{ color: "#14CE9E", fontSize: "45px" }} />
+                <StyledHouseIcon fontSize="40px" />
               </Button>
             </Grid>
-            <Typography
-              sx={{
-                fontWeight: "bold",
-                color: "black",
-                fontSize: "25px",
-                ml: "35px",
-                mr: "35px",
-              }}
-            >
-              All Ads
-            </Typography>
-            <TextField
+            <Title>
+              <Typography>All Ads</Typography>
+            </Title>{" "}
+            <Input
               id="outlined-size-small"
               variant="outlined"
               value={inputValue}
               onChange={handleChange}
               placeholder="Search for ads..."
               type="text"
-              sx={{
-                width: "600px",
-                mt: "5px",
-                mb: "5px",
-                height: "60px",
-                mr: "35px",
-              }} // Adjusted height
               InputProps={{
                 startAdornment: (
                   <InputAdornment position="start">
@@ -76,53 +150,59 @@ const Home = () => {
               }}
             />
             <Grid sx={{ mr: "45px" }}>
-              <Button>
-                <LanguageIcon
-                  sx={{
-                    color: "#14CE9E",
-                    fontSize: "45px",
-                    border: "1px solid #14CE9E", // Added border style
-                  }}
-                />
+              <Button onClick={handleLanguageClick}>
+                <StyledLanguageIcon fontSize="40px" />
               </Button>
             </Grid>
-            <Grid
-              onClick={handleLoginClick}
-              sx={{
-                color: "green",
-                fontSize: "45px",
-                border: "1px solid #14CE9E",
-                width: "174px",
-                height: "51px",
-                display: "flex",
-                alignItems: "center", // Center align the text vertically
-                justifyContent: "center",
-                mr: "35px", // Added border style
-              }}
-            >
-              <Button sx={{ color: "black" }}>Log in</Button>
-            </Grid>
-            <Grid
-              sx={{
-                backgroundColor: "#14CE9E",
-                fontSize: "45px",
-                border: "1px solid #14CE9E",
-                width: "174px",
-                height: "51px",
-                display: "flex",
-                alignItems: "center", // Center align the text vertically
-                justifyContent: "center",
-                ml: "35px", // Center align the text horizontally
-              }}
-            >
+            <LoginGrid onClick={handleLoginClick}>
+              <LoginB>Log in</LoginB>
+            </LoginGrid>
+            <PostGrid onClick={handleAddPostClick}>
               <Button sx={{ color: "white" }}>Add Post</Button>
-            </Grid>
-          </Grid>
+            </PostGrid>
+          </MainContainner>
         </Toolbar>
-      </AppBar>
+      </AppBarContainer>
+
+      {languageOpen && (
+        <Grid
+          sx={{
+            backgroundColor: "#14CE9E",
+            fontSize: "45px",
+            border: "1px solid #14CE9E",
+            width: "174px",
+            height: "51px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            ml: "35px",
+          }}
+        >
+          <Button sx={{ color: "white" }}>Language</Button>
+        </Grid>
+      )}
+
       {loginOpen && (
         <Login handleClose={handleLoginClose} handleOpen={handleLoginClick} />
       )}
+
+      {addPostOpen && (
+        <Grid
+          sx={{
+            backgroundColor: "#14CE9E",
+            fontSize: "45px",
+            border: "1px solid #14CE9E",
+            width: "174px",
+            height: "51px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            ml: "35px",
+          }}
+        >
+          <Button sx={{ color: "white" }}>Add Post</Button>
+        </Grid>
+      )} */}
     </div>
   );
 };
---
+++ frontend/src/Components/ListView/SearchBar.js
@@ -0,0 +1,258 @@
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import HouseIcon from "@mui/icons-material/House";
+import LanguageIcon from "@mui/icons-material/Language";
+import Button from "@mui/material/Button";
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: "white",
+  border: "1px solid #E9EFED", // Change the border color here
+  "&:hover": {
+    backgroundColor: "#F1F9F6",
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+const StyledAppBar = styled(AppBar)(({ theme }) => ({
+  backgroundColor: "white", // Change the background color here
+  "& .MuiAppBar-root": {
+    boxShadow: "none",
+  },
+}));
+
+const StyledIcon = styled(SearchIcon)(({ theme }) => ({
+  color: "#CECECE",
+}));
+const StyledHouseIcon = styled(HouseIcon)(({ theme }) => ({
+  color: "#14ce9e",
+}));
+
+const StyledLanguageIcon = styled(LanguageIcon)(({ theme }) => ({
+  color: "#14ce9e",
+  fontSize: "40px",
+  width: "100%",
+  marginRight: "30px",
+}));
+
+const StyledLogin = styled(Button)(({ theme }) => ({
+  color: "#14ce9e",
+  border: " 1px solid #14ce9e",
+  width: "100%",
+  [theme.breakpoints.up("md")]: {
+    width: "20ch",
+  },
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  "&:hover": {
+    backgroundColor: "#14ce9e",
+    color: "white",
+  },
+}));
+
+const StyledAdd = styled(Button)(({ theme }) => ({
+  marginLeft: "30px",
+  backgroundColor: "#14ce9e",
+  border: "1px solid #14ce9e",
+  width: "100%",
+  [theme.breakpoints.up("md")]: {
+    width: "20ch",
+  },
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "white",
+  "&:hover": {
+    color: "#14ce9e",
+  },
+}));
+
+export default function SearchBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton size="large" color="inherit">
+          <MailIcon />
+        </IconButton>
+        <p>Login</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton size="large" color="inherit">
+          <NotificationsIcon />
+        </IconButton>
+        <p>SignUp</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <StyledAppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <StyledHouseIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: "none", sm: "block", color: "black" } }}
+          >
+            All Ads
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <StyledIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <Button>
+              <StyledLanguageIcon />
+            </Button>
+            <StyledLogin>Login</StyledLogin>
+            <StyledAdd>Add Post</StyledAdd>
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </StyledAppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
+  );
+}
